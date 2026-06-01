59b14780c85e1d489dc502319bd93ff9f303f0d4
hesen <hesen123@gmail.com>
2026-06-01 14:56:33 +0400

    add

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Divide two numbers."""
+    return a / b
